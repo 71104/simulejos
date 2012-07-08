dfe49ca60940f133ef776404206badc562130097
Alberto La Rocca <a71104@gmail.com>
2012-07-08 03:25:38 +0200

    ~

--- TestRobots/scripts/test1.js
+++ TestRobots/scripts/test1.js
@@ -4,17 +4,21 @@ var wheelDiameter = 1;
 function tick(daa, dab, dac) {
 	var dsa = daa * wheelDiameter * Math.PI;
 	var dsb = dab * wheelDiameter * Math.PI;
-	if (Math.abs(dsa) > Math.abs(dsb)) {
-		var angle = (dsa - dsb) / wheelSpan;
-		var radius = dsa * wheelSpan / (dsa - dsb) - wheelSpan / 2;
-		robot.moveBy(radius - Math.cos(angle) * radius, 0, Math.sin(angle) * radius);
-		robot.rotateBy(0, 1, 0, -angle);
-	} else if (Math.abs(dsb) > Math.abs(dsa)) {
-		var angle = (dsb - dsa) / wheelSpan;
-		var radius = dsb * wheelSpan / (dsb - dsa) - wheelSpan / 2;
-		robot.moveBy(Math.cos(angle) * radius - radius, 0, Math.sin(angle) * radius);
-		robot.rotateBy(0, 1, 0, angle);
+	if (dsa * dsb < 0) {
+		// TODO
 	} else {
-		robot.moveBy(0, 0, dsa);
+		if (Math.abs(dsa) > Math.abs(dsb)) {
+			var angle = (dsa - dsb) / wheelSpan;
+			var radius = wheelSpan * ((dsa + dsb) / (dsa - dsb)) / 2;
+			robot.moveBy(radius - Math.cos(angle) * radius, 0, Math.sin(angle) * radius);
+			robot.rotateBy(0, 1, 0, -angle);
+		} else if (Math.abs(dsb) > Math.abs(dsa)) {
+			var angle = (dsb - dsa) / wheelSpan;
+			var radius = wheelSpan * ((dsa + dsb) / (dsb - dsa)) / 2;
+			robot.moveBy(Math.cos(angle) * radius - radius, 0, Math.sin(angle) * radius);
+			robot.rotateBy(0, 1, 0, angle);
+		} else {
+			robot.moveBy(0, 0, dsa);
+		}
 	}
 }
