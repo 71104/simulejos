fcddf26b7c3d42c963db934d10380408ee74cf34
71104 <a71104@gmail.com>
2012-11-28 13:49:43 +0100

    ~

--- TestRobots/src/it/uniroma1/di/simulejos/test/robots/Probe2.java
+++ TestRobots/src/it/uniroma1/di/simulejos/test/robots/Probe2.java
@@ -7,14 +7,14 @@ import lejos.nxt.SensorPort;
 
 public final class Probe2 {
 	public static void main(String[] arguments) {
-		Motor.A.setSpeed(50);
-		Motor.B.setSpeed(50);
+		Motor.A.setSpeed(200);
+		Motor.B.setSpeed(200);
 		Motor.A.forward();
 		Motor.B.forward();
 		final LightSensor sensor = new LightSensor(SensorPort.S1);
 		final Object blocker = new Object();
 		while (true) {
-			if (sensor.readValue() > 10) {
+			if (sensor.readNormalizedValue() >= 512) {
 				LCD.bitBlt(null, LCD.SCREEN_WIDTH, LCD.SCREEN_HEIGHT, 0, 0, 0,
 						0, LCD.SCREEN_WIDTH, LCD.SCREEN_HEIGHT, LCD.ROP_CLEAR);
 			} else {
